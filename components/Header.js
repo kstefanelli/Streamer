bcea95f3f3cd12e210c3c7c9f18c6a208b32afcf
Kristina Stefanelli <kristinastefanelli@gmail.com>
2022-02-25 15:31:17 -0500

    style: Update Header Icons

--- components/Header.js
+++ components/Header.js
@@ -1,22 +1,22 @@
 import {
-  BadgeCheckIcon,
   CollectionIcon,
   HomeIcon,
-  LightningBoltIcon,
   SearchIcon,
   UserIcon,
+  HeartIcon,
 } from '@heroicons/react/outline';
 import HeaderItem from './HeaderItem';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 function Header() {
+  const router = useRouter();
   return (
     <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
       <div className="flex flex-grow justify-evenly max-w-2xl">
         <HeaderItem title="HOME" Icon={HomeIcon} />
-        <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
-        <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
         <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
+        <HeaderItem title="FAVORITES" Icon={HeartIcon}/>
         <HeaderItem title="SEARCH" Icon={SearchIcon} />
         <HeaderItem title="ACCOUNT" Icon={UserIcon} />
       </div>

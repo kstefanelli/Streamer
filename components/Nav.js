4f571d52d2f32323242a577b6dfabdcab7b4514f
Kristina Stefanelli <kristinastefanelli@gmail.com>
2022-02-25 15:34:31 -0500

    style: Update Active Text Nav Color

--- components/Nav.js
+++ components/Nav.js
@@ -11,7 +11,7 @@ function Nav() {
             key={key}
             onClick={() => router.push(`/?genre=${key}`)}
             className="last:pr-24 cursor-pointer transition duration-100 
-            transform hover:scale-125 hover: text-white active:text-red-500"
+            transform hover:scale-125 hover: text-white active:text-teal-500"
           >
             {title}
           </h2>
